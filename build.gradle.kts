c6ffbf79c00c609fec466fdedb331c402391140b
thegr8manef <jedidimanef@gmail.com>
2024-03-26 13:04:43 +0100

    third commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
+    id("maven-publish")
 
+}
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4") // Use the appropriate version
+    }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinCocoapods)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 
 }
 
@@ -27,7 +28,7 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
